ce80e3c6c4b0933e95b235611e0b0cbf31556c73
Relster27 <hatcherwaas@gmail.com>
2026-03-08 18:31:34 +0700

    Added fuzzer

---
+++ fuzz.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+TARGET = './vuln'
+HOST = ''
+PORT = 1337
+
+elf = ELF(TARGET)
+
+context.arch = 'amd64'
+gdb_script = f"""
+  b *main
+  c
+  c
+"""
+# context.log_level = 'DEBUG'
+
+if args.GDB:
+  # context.terminal = ['tmux', 'splitw', '-h']  
+  context.terminal = [
+    'wt.exe', '-w', '0', 'new-tab', '--', 'bash', '-lc'
+  ]
+  p = gdb.debug(TARGET, gdbscript=gdb_script)
+elif not args.REMOTE:
+  p = process(TARGET)
+else:
+  p = remote(HOST, PORT)
+
+# ===================================== #
+
+p.sendline(b'A' * 16 + b'B' * 8 + b'C' * 8)
+
+p.interactive()
